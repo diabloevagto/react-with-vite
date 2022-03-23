d62ec2324b7b8090e73dc4d7cdcb478345b2c444
wi <diabloevagto@gmail.com>
2022-03-23 16:04:35 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  eslintIntegration: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+};
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+          <button
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
